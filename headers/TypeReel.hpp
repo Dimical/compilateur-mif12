ffcfb1ba36cd0aa34f8d916ae96897cb4a587b0c
afkir.bilal <afkir.bilal@gmail.com>
2011-11-04 16:08:33 +0000

    type.hpp blablabla

---
+++ headers/TypeReel.hpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   TypeReel.hpp
+ * Author: bilal
+ *
+ * Created on 4 novembre 2011, 16:52
+ */
+
+#ifndef TYPEREEL_HPP
+#define	TYPEREEL_HPP
+
+class TypeReel {
+public:
+    TypeReel();
+    TypeReel(const TypeReel& orig);
+    virtual ~TypeReel();
+private:
+
+};
+
+#endif	/* TYPEREEL_HPP */
+
