f22cd699712b6884998ab6e57f454dc3b6bb5b23
faurevincent.p <faurevincent.p@gmail.com>
2011-11-12 10:40:18 +0000

    Mes modifications. Elle sont dans le dossier temp

---
+++ temp/Symbole.hpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   Symbole.hpp
+ * Author: p0700785
+ *
+ * Created on 14 octobre 2011, 10:11
+ */
+
+#ifndef SYMBOLE_HPP
+#define	SYMBOLE_HPP
+
+#include "TableId.hpp"
+#include "Type.hpp"
+#include <string>
+
+class Symbole{
+
+protected:
+    Type T;
+    
+public:
+    // classe abstraite
+    Symbole();
+    Symbole(Type t);
+    Symbole(const Symbole& orig);
+    virtual ~Symbole();
+    std::string toString();
+
+};
+
+#endif	/* SYMBOLE_HPP */
+
---
+++ temp/SymboleVar.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   SymboleVar.cpp
+ * Author: bilal
+ * 
+ * Created on 21 octobre 2011, 16:48
+ */
+
+#include "../headers/SymboleVar.hpp"
+
+SymboleVar::SymboleVar(): Symbole() {
+}
+
+SymboleVar::SymboleVar(Type t) {
+    this->T=t;
+}
+
+SymboleVar::SymboleVar(const SymboleVar& orig) {
+}
+
+SymboleVar::~SymboleVar() {
+}
+
+//std::string toString(){
+//    return("");
+//}
+
