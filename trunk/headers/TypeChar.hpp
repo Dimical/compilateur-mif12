16f457af6ff3647a95e8baf5fa1590f7694e0581
faurevincent.p@gmail.com <faurevincent.p@gmail.com@b56ee055-0fb1-3006-f0c2-f751efe08544>
2011-11-24 18:12:12 +0000

    typechar

---
+++ trunk/headers/TypeChar.hpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   TypeChar.hpp
+ * Author: bart
+ *
+ * Created on 19 novembre 2011, 22:41
+ */
+
+#ifndef TYPECHAR_HPP
+#define	TYPECHAR_HPP
+
+#include "Type.hpp"
+
+class TypeChar : public Type
+{
+public:
+    TypeChar();
+    TypeChar(const TypeChar& orig);
+    ~TypeChar();
+        std::string TypeChar::getClass();
+    bool operator ==(Type const& T1);
+private:
+
+};
+
+#endif	/* TYPECHAR_HPP */
+
