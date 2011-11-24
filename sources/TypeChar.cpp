4a03253f35811e81373a4afc21df738023d9cd79
faurevincent.p <faurevincent.p@gmail.com>
2011-11-24 18:13:58 +0000

    typechar cpp

---
+++ sources/TypeChar.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   TypeChar.cpp
+ * Author: bart
+ * 
+ * Created on 19 novembre 2011, 22:39
+ */
+
+#include "../headers/TypeChar.hpp"
+
+TypeChar::TypeChar() {
+}
+
+TypeChar::TypeChar(const TypeChar& orig) {
+}
+
+TypeChar::~TypeChar() {
+}
+
+std::string TypeChar::getClass() {
+    return"char";
+}
