1fbfab187fd49de31aef02d9642230b9c24de10b
afkir.bilal@gmail.com <afkir.bilal@gmail.com@b56ee055-0fb1-3006-f0c2-f751efe08544>
2011-12-05 17:39:26 +0000

    fichier de test a  tester

--- trunk/tests/test01-3adresses.pas
+++ trunk/tests/test01-3adresses.pas
@@ -3,6 +3,19 @@ var a : integer;
     b : integer;
     c : integer;
     d : integer;
+    e : integer;
+    f : integer;
+    toto : boolean;
+    titi : boolean;
+    tutu : boolean;
+    i : integer;
 begin
-    a := b + c + d;
+    a := b - c- d - e - f;
+    tutu := NOT titi;
+
+    if a = b then
+        a := c;
+
+    for i := 2 to 4 do
+        a := i;
 end.
